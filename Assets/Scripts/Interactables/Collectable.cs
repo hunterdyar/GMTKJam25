7d5e1c589c3047d962d9a4170187106c360602d9
hunterdyar <bloop@hdyar.com>
2025-08-01 11:23:15 -0400

    collectable code works with batteries

--- Assets/Scripts/Interactables/Collectable.cs
+++ Assets/Scripts/Interactables/Collectable.cs
@@ -8,12 +8,7 @@ namespace GMTK
 		private MeshRenderer _meshRenderer;
 		//for tracking when we need to switch visuals, not a source of truth.
 		private bool _hasBeenCollected;
-
-		protected override void Awake()
-		{
-			base.Awake();
-			_meshRenderer = GetComponent<MeshRenderer>();
-		}
+		
 
 		void Start()
 		{
@@ -31,7 +26,10 @@ namespace GMTK
 		private void SetCollected(bool collected)
 		{
 			_hasBeenCollected = collected;
-			_meshRenderer.enabled = !collected;
+			foreach (Transform child in transform)
+			{
+				child.gameObject.SetActive(!collected);
+			}
 		}
 	}
 }
